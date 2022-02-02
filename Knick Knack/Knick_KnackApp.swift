f9f8b5624436ae9a31fe0f15177dcb614d286420
Jordan <jordanbhuffaker@gmail.com>
2022-02-01 21:13:34 -0700

    Initial Commit

---
+++ Knick Knack/Knick_KnackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Knick_KnackApp.swift
+//  Knick Knack
+//
+//  Created by Jordan Huffaker on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Knick_KnackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
